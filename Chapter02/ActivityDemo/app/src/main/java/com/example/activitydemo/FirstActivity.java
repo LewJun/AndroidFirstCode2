3f587386f5a1101ec32bf929206bfc07e606023d
LewJun <lewjun072@sina.com>
2018-01-02 23:36:07 +0800

    使用菜单第二步：重写OnCreateOptionsMenu方法，第三步：响应菜单项点击事件。使用finish方法销毁活动。

--- Chapter02/ActivityDemo/app/src/main/java/com/example/activitydemo/FirstActivity.java
+++ Chapter02/ActivityDemo/app/src/main/java/com/example/activitydemo/FirstActivity.java
@@ -2,6 +2,8 @@ package com.example.activitydemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -32,5 +34,40 @@ public class FirstActivity extends AppCompatActivity {
                 Toast.makeText(FirstActivity.this, "hello world", Toast.LENGTH_SHORT).show();
             }
         });
+
+        Button btn_finish = findViewById(R.id.btn_finish);
+        btn_finish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                通过调用finish方法销毁一个活动
+                finish();
+            }
+        });
+    }
+
+    // 使用菜单第二步，重写onCreateOptionsMenu方法，为菜单绑定菜单项
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // 将之前声明的菜单项添加到menu
+        getMenuInflater().inflate(R.menu.mnu_first, menu);
+        // 返回true表示允许菜单显示出来，否则不能显示。
+        return true;
+    }
+
+    // 使用菜单第三步，响应菜单点击事件
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_item:
+                Toast.makeText(this, "Add", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this, "Remove", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+//        返回true表示点击后，点击事件不会向下传递，而返回false会向下传递
+        return true;
     }
 }
