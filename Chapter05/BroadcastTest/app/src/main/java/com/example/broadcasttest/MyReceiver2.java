727f9e272a80a7f5b8d52a70d2cdb8f86bbaf68b
LewJun <lewjun072@sina.com>
2018-01-14 23:36:26 +0800

    静态注册的广播不能弹出对话框这样的UI控件

--- Chapter05/BroadcastTest/app/src/main/java/com/example/broadcasttest/MyReceiver2.java
+++ Chapter05/BroadcastTest/app/src/main/java/com/example/broadcasttest/MyReceiver2.java
@@ -3,6 +3,7 @@ package com.example.broadcasttest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 
 public class MyReceiver2 extends BroadcastReceiver {
@@ -11,5 +12,14 @@ public class MyReceiver2 extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.d(TAG, "onReceive: " + intent.getAction());
+
+// java.lang.RuntimeException: Unable to start receiver com.example.broadcasttest.MyReceiver2:
+// java.lang.IllegalStateException: You need to use a Theme.AppCompat
+// theme (or descendant) with this activity.
+        // 静态注册的广播不能弹出对话框这样的UI控件。
+        /*new AlertDialog.Builder(context)
+                .setTitle("title")
+                .setMessage("message")
+                .show();*/
     }
 }
