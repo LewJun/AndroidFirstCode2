7396b1b8931d7fb7652d7e74d0526ae5400f11d5
LewJun <lewjun072@sina.com>
2018-01-31 10:36:01 +0800

    PopupMenu的使用

--- Chapter03/UICustomViews/app/src/main/java/com/example/uicustomviews/MainActivity.java
+++ Chapter03/UICustomViews/app/src/main/java/com/example/uicustomviews/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.uicustomviews;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.PopupMenu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +20,21 @@ public class MainActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.hide();
         }
+
+        findViewById(R.id.btn_show_popup_menu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PopupMenu popupMenu = new PopupMenu(v.getContext(), v);
+                popupMenu.getMenuInflater().inflate(R.menu.menu_head, popupMenu.getMenu());
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        Toast.makeText(MainActivity.this, item.getTitle(), Toast.LENGTH_SHORT).show();
+                        return false;
+                    }
+                });
+                popupMenu.show();
+            }
+        });
     }
 }
