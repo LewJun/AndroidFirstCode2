ece46374de43602e707f644fa2e4d12355404e1b
LewJun <lewjun072@sina.com>
2018-03-01 16:06:38 +0800

    使用clip标签可以对drawable进行裁剪

--- Chapter03/DroidStyle/app/src/main/java/com/example/droidstyle/AnimationActivity.java
+++ Chapter03/DroidStyle/app/src/main/java/com/example/droidstyle/AnimationActivity.java
@@ -2,12 +2,39 @@ package com.example.droidstyle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.SeekBar;
 
 public class AnimationActivity extends AppCompatActivity {
+    private ImageView iv_clip;
+    private SeekBar sb_clip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_animation);
+
+        iv_clip = findViewById(R.id.iv_clip);
+//        通过设置level值控制裁剪多少，level取值范围为0~10000，默认为0，表示完全裁剪，图片将不可见；10000则完全不裁剪，可见完整图片。
+        iv_clip.setImageLevel(5000);
+
+        sb_clip = findViewById(R.id.sb_clip);
+//        通过滑块改变图片的level值
+        sb_clip.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                iv_clip.setImageLevel(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
