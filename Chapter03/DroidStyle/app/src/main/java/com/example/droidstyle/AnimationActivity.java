2700ed9fb61389317de92e332a279f57be75fad6
LewJun <lewjun072@sina.com>
2018-03-05 13:35:07 +0800

    objectAnimator 对象动画 可指定具体属性名

--- Chapter03/DroidStyle/app/src/main/java/com/example/droidstyle/AnimationActivity.java
+++ Chapter03/DroidStyle/app/src/main/java/com/example/droidstyle/AnimationActivity.java
@@ -1,6 +1,7 @@
 package com.example.droidstyle;
 
 import android.animation.AnimatorInflater;
+import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.TransitionDrawable;
@@ -127,5 +128,44 @@ public class AnimationActivity extends AppCompatActivity {
         });
         // 启动动画
         valueAnimator.start();
+    }
+
+    public void onScaleWidth2(View view) {
+        // 得到当前控件的宽度
+        final int viewWidth = view.getWidth();
+        // 将目标view进行包装
+        ViewWrapper viewWrapper = new ViewWrapper(view, viewWidth);
+        // 通过AnimatorInflater加载Animator类实例动画
+        ObjectAnimator objectAnimator = (ObjectAnimator) AnimatorInflater.loadAnimator(this, R.animator.object_animator);
+        // 设置动画的目标对象为包装后的view
+        objectAnimator.setTarget(viewWrapper);
+        // 启动动画
+        objectAnimator.start();
+    }
+
+    private static class ViewWrapper {
+        private View targetView;
+        private int maxWidth;
+
+        private ViewWrapper(View targetView, int maxWidth) {
+            this.targetView = targetView;
+            this.maxWidth = maxWidth;
+        }
+
+        /**
+         * 得到View实际的宽度
+         */
+        public int getWidth() {
+            return targetView.getLayoutParams().width;
+        }
+
+        /**
+         * 设置View实际的宽度 = 最大宽度 * 动画宽度比例值 / 100
+         * @param widthValue 动画宽度比例值
+         */
+        public void setWidth(int widthValue) {
+            targetView.getLayoutParams().width = maxWidth * widthValue / 100;
+            targetView.requestLayout();
+        }
     }
 }
